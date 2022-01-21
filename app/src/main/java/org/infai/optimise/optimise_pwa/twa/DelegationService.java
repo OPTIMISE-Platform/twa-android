6034aad234b5ed2675c2a86b90018f416b59f9a2
franzmueller <franzmueller@users.noreply.github.com>
2022-01-21 14:45:32 +0100

    init

---
+++ app/src/main/java/org/infai/optimise/optimise_pwa/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package org.infai.optimise.optimise_pwa.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
